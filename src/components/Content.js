d83983fa97f5595425d0e1ee35d1a04a6a77e3a4
Shivani Sharma <s.shiv619@gmail.com>
2020-08-09 12:31:14 +0530

    zone and geoence around mumbai added

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,10 +3,17 @@ import {
 	Map, 
 	GoogleApiWrapper, 
 	Marker,
-	InfoWindow,  
+	InfoWindow,
+	Polygon,
 } from 'google-maps-react';
 import 'styles/content.scss';
 
+const triangleCoords = [
+  { lat: 19.07366956, lng: 72.84749221 },
+  { lat: 19.05400526, lng: 72.88481138 },
+  { lat: 19.09602431, lng: 72.88378142 },
+  { lat: 19.07366956, lng: 72.84749221 }
+];
 
 function Content({
 	google,
@@ -23,12 +30,20 @@ function Content({
       <div className="col-12 d-flex justify-content-center p-4">
 	      <Map
 	        google={google}
-	        zoom={12}
+	        zoom={13}
 	        className="google-map"
-	        initialCenter={{ lat: 19.0760, lng: 72.8777}}
+	        initialCenter={{ lat: 19.0760, lng: 72.8777 }}
 	      >
 	        <Marker
 	          name={'Mumbai'}
+	        />
+	        <Polygon
+	          paths={triangleCoords}
+	          strokeColor="#FF0000"
+	          strokeOpacity={0.8}
+	          strokeWeight={2}
+	          fillColor="#FF0000"
+	          fillOpacity={0.35} 
 	        />
 	      </Map>
 	      </div>
